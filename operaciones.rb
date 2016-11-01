e638456100e86cb255d62bdefd50057cc9287482
codeamac12 <codeamac12@gmail.com>
2016-11-01 13:52:25 -0600

    resuelto

--- operaciones.rb
+++ operaciones.rb
@@ -1,8 +1,8 @@
 var1 = 20
 var2 = 10
 
-resta = #Aquí asigna la operación aritmética
-multiplicacion = resta * var1 + var2
+resta = var1 - var2
+multiplicacion = resta * (var1 + var2)
 division = multiplicacion / 3
 p division == 100
 #=>true
