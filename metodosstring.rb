a8a6574d795b9fbbf1cbbf4a6cfc21dabe4061cf
codeamac16 <codeamac16@gmail.com>
2016-11-02 14:08:25 -0600

    resuelto

---
+++ metodosstring.rb
@@ -0,0 +1,24 @@
+
+payment = 'Welcome, your Visa Credit Card has been processed'
+
+if payment.include? "Visa"
+  p "Credit Card has been Charged"
+
+else "We only accept visa credit card"
+end
+
+
+
+name = "Daniel"
+age = 18
+
+
+if name == "Daniel" && age >= 18
+  p "Welcome"
+else p "No eres Mayor de Edad"
+end
+
+
+
+
+
