8c9774b051f5ec51e67798d0b674452d1b384a7d
codeamac16 <codeamac16@gmail.com>
2016-11-02 12:37:55 -0600

    Hasta Substring

--- estructurascontrol.rb
+++ estructurascontrol.rb
@@ -72,7 +72,55 @@ p large("Hola vamos a la comida") == "HOLA VAMOS A LA COMIDA"
 p large("Es hora de dormir") == "Es hora de dormir"
 
 
+
+
+
+
+
+
+def range(num)
+
+
+
+case num
+when 10..50
+  "#{num} is between 0 and 50"
+when 51..100
+  "#{num} is between 51 and 100"
+
+else
+  "#{num} is above 120"
+end
+
+
+
+
+end
+
+p range(10) == "10 is between 0 and 50"
+p range(34) == "34 is between 0 and 50"
+p range(79) == "79 is between 51 and 100"
+p range(67) == "67 is between 51 and 100"
+p range(54) == "54 is between 51 and 100"
+p range(120) == "120 is above 100"
+
 =end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
