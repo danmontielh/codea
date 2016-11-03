8d6b962db3356f17fdb8a22c19c2f753e48b6a5c
codeamac16 <codeamac16@gmail.com>
2016-11-03 13:11:37 -0600

    Resuelto

---
+++ numentradas.rb
@@ -0,0 +1,26 @@
+def user
+
+print "Escribe "
+input = gets.chomp
+
+count = 0
+
+
+while input != "Ya"
+
+print "Escribe "
+input = gets.chomp
+
+count += 1
+
+
+
+end
+
+p "El numero de veces escrito: #{count}"
+
+end
+
+user 
+
+
