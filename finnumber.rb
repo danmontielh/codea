9e92f11f791963f341c7b0c19c2285086b46ba57
Dan Mon <Dan Mon>
2016-11-07 18:08:04 -0600

    Resuelto

--- finnumber.rb
+++ finnumber.rb
@@ -1,7 +1,3 @@
-def numbers()
-  
-end
-
 =begin
 Define el método numbers que recibe un arreglo de números y un número. Regresa true si el número dado aparece en la lista.
 
@@ -12,3 +8,19 @@ p numbers([11, 33, 55, 77, 99, 11], 33) == true
 p numbers([20, 43, 55, 77, 99, 40], 43) == true
 
 =end
+
+
+def numbers(arr,num)
+
+	arr.each do |list|  
+		while list == num
+			return true
+		end
+
+	end
+end
+
+p numbers([1, 3, 5, 7, 9, 11], 5) == true
+p numbers([1, 50, 24, 7, 9, 100], 25) == false
+p numbers([11, 33, 55, 77, 99, 11], 33) == true
+p numbers([20, 43, 55, 77, 99, 40], 43) == true
