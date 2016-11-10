3015f5497c397ec8fea4461300044bdf5a1f9b71
codeamac16 <codeamac16@gmail.com>
2016-11-10 12:00:41 -0600

    Resuelto

---
+++ hash.rb
@@ -0,0 +1,34 @@
+catalogo_tienda = {
+
+"camisa" => 5,
+"playera" => 3,
+"short" => 7,
+"pantalón" => 2
+
+}
+
+
+catalogo_tienda["camisa"]
+catalogo_tienda["calcetines"] = 3
+
+=begin
+catalogo_tienda = Hash.new 
+catalogo_tienda[:camisa] = 5
+catalogo_tienda[:playera] = 3
+catalogo_tienda[:short] = 7
+catalogo_tienda[:pantalon] = 2
+=end
+
+new_hash = {}
+canasta_de_frutas = {
+  "mango" => 10,
+  "fresa" => 20,
+  "uva" => 30
+}
+
+canasta_de_frutas["pera"] = 40
+
+fruta_seleccionada = canasta_de_frutas["mango"]
+
+
+
