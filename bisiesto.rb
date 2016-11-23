e6d89f29b32c84017826c9b85857bfe45d6de17a
Dan Mon <Dan Mon>
2016-11-23 14:26:59 -0600

    Resuelto

--- bisiesto.rb
+++ bisiesto.rb
@@ -1,8 +1,28 @@
 def leap_year?(year)
 
-year % 400 == 0 ? true ? year % 4
+if year % 400 == 0
+	true
+elsif year % 100 == 0
+	false
+elsif year % 4 == 0
+	true
+else
+	false
+	
+	
+	
+end
+ 			
+
 
 
 end
 
-p leap_year?(1704) 
+p leap_year?(1900) == false
+p leap_year?(1988) == true
+p leap_year?(1989) == false
+p leap_year?(1992) == true
+p leap_year?(2000) == true
+p leap_year?(2001) == false
+p leap_year?(1600) == true  
+p leap_year?(1704) == true
