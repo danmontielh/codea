c54c272db5f6850dc47bf1ea0c1dae03e1938938
codeamac15 <codeamac15@gmail.com>
2016-12-14 12:36:10 -0600

    resuelto

---
+++ oriented.rb
@@ -0,0 +1,69 @@
+class Cat
+end
+
+class Dug
+end
+
+class Dog
+end
+
+class Dolphin
+end
+
+
+
+# Crear objetos
+
+class cat
+end
+
+
+obj_one = cat.new
+obj_two = cat.new
+obj_three = cat.new
+
+
+class car
+end
+
+obj_one = car.new
+obj_two = car.new
+obj_three = car.new
+
+class window
+end
+
+obj_one = window.new
+obj_two = window.new
+obj_three = window.new
+
+class water
+end
+
+obj_one = water.new
+obj_two = water.new
+obj_three = water.new
+
+
+class fruit
+end
+
+obj_one = fruit.new
+obj_two = fruit.new
+obj_three = fruit.new
+
+class vegetable
+end
+
+obj_one = vegetable.new
+obj_two = vegetable.new
+obj_three = vegetable.new
+
+class person
+end
+
+obj_one = person.new
+obj_two = person.new
+obj_three = person.new
+
+
