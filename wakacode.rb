9844e6d9e9213b0a7a4b18741ade5c990421a72c
codeamac11 <codeamac11@gmail.com>
2016-12-15 14:30:25 -0600

    casi resuelto

--- wakacode.rb
+++ wakacode.rb
@@ -1,47 +1,62 @@
 class WakaWaka
 
+  @@symbol = {
+        '<' => 'Waka',
+        '>' => 'waka',
+        '!' => 'bang',
+        "'" => 'tick',
+        '#' => 'hash',
+        '^' => 'Caret',
+        '"' => 'quote',
+        '`' => 'back-tick',
+        '$' => 'dollar',
+        '-' => 'dash',
+        '=' => 'equal',
+        '@' => 'at',
+        '_' => 'underscore',
+        '%' => 'Percent',
+        '~' => 'tilde',
+        '4' => 'four',
+        '*' => 'splat',
+    }
+
   def initialize(code)
     @code = code
+    @search_result = []
   end
 
-  dict = {
-      '<' => 'Waka',
-      '>' => 'waka',
-      '!' => 'bang',
-      "'" => 'tick',
-      '#' => 'hash',
-      '^' => 'Caret',
-      '"' => 'quote',
-      '`' => 'back-tick',
-      '$' => 'dollar',
-      '-' => 'dash',
-      '!' => 'Bang',
-      '=' => 'equal',
-      '@' => 'at',
-      '_' => 'underscore',
-      '%' => 'Percent',
-      '~' => 'tilde',
-      '4' => 'four',
-      '*' => 'splat',
-  }
+  def search_translate(code)
+    @@symbol.each do |symbol, value|
+      @search_result << value if symbol == code
+    end
+  end
 
-  def traslate
-
-  @code.each_with_index do |symbol,index|
-
-    print "#{symbol} \t"
-    
-    
+  def print_result
+     @search_result.each do |value|
+       printf "%8s", value
+     end
   end
 
 
-  end
 
+  def translate
+
+   
+    @code[0].each do |code|
+      printf "%6s", code
+      search_translate(code)
+    end
+
+    puts
+    print_result
+    puts
+  end
 
 
 
 end
 
-waka_code = WakaWaka.new(["<", ">", "!", "*", "'", "'", "#", "^", '"', "`", "$", "$", "-", "!", "*", "=", "@", "$", "_", "%", "*", "<", ">", "~", "#", "4"])
+waka_code = WakaWaka.new([["<", ">", "!", "*", "'", "'", "#"], "^", '"', "`", "$", "$", "-", "!", "*", "=", "@", "$", "_", "%", "*", "<", ">", "~", "#", "4"])
 
-waka_code.traslate
+
+waka_code.translate
